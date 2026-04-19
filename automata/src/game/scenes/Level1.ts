dbc9e7568dbd15813f6592dac6bf5df66f0edaa6
Demomaker <mike01bedard@gmail.com>
2026-04-18 20:30:02 -0400

    create level 1 file

---
+++ automata/src/game/scenes/Level1.ts
@@ -0,0 +1,18 @@
+export class Level1 extends Scene
+{
+    constructor()
+    {
+        super('Level1')
+    }
+
+    preload()
+    {
+        this.load.setPath('assets');
+    }
+
+    create()
+    {
+
+    }
+
+}
